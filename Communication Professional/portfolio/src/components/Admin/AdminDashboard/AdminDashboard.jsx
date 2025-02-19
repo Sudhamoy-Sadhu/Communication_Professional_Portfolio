15559766fbe04c3af31d61b558951c4d0d66fee0
Sudhamoy sadhu <sudhamoy.sadhu2004@gmail.com>
2025-02-19 23:06:36 +0530

    Added TotalReviews

--- Communication Professional/portfolio/src/components/Admin/AdminDashboard/AdminDashboard.jsx
+++ Communication Professional/portfolio/src/components/Admin/AdminDashboard/AdminDashboard.jsx
@@ -22,7 +22,7 @@ function AdminDashboard() {
         const totalReviews = async () => {
           try {
             const response = await axios.get(API_GET_TOTAL_REVIEWS);
-            setTotalReviews(response.data);
+            setTotalReviews(response.data.totalReviews);
           } catch (error) {
             console.error("Error fetching total number of reviews:", error);
           }
